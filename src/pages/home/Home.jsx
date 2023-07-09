5cb542544afe3727218e92c7e54bdef895cbacfc
ibrahzizo360 <92813019+ibrahzizo360@users.noreply.github.com>
2023-07-09 10:51:04 +0000

    commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import Sidebar from '../../components/sidebar/Sidebar'
+import './home.scss'
+
+const Home = () => {
+  return (
+    <Sidebar/>
+  )
+}
+
+export default Home
---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const single = () => {
+  return (
+    <div>single</div>
+  )
+}
+
+export default single
